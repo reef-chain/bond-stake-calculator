6d451ea30908613c7d2881af87c493d423ec3a13
Anukul Pandey <anukul.030601@gmail.com>
2023-09-22 11:05:16 +0530

    feat: calculate sum of total staked

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const {argv} = require('./utils/args');
 const {fetchStakeEvents} = require('./utils/graphql')
+const {BigNumber} = require('bignumber.js')
 
 const contractAddress = argv['contract-address'];
 const topic = argv['topic'];
 
-const response = fetchStakeEvents(contractAddress,topic);
-response.then(data=>{
-    const evmEvents = data.data.evmEvents;
-    for(let i=0;i<evmEvents.length;i++){
-        console.log(evmEvents[i].dataParsed.args[1])
+let hasNextPage = true;
+let after = "0";
+
+async function fetchEvents() {
+    let totalStaked = BigNumber(0);
+    let processedCount = 0;
+    let totalCount = 0;
+
+    while (hasNextPage) {
+      try {
+        const res = await fetchStakeEvents(contractAddress, topic, after);
+        const data = res.data.evmEventsConnection;
+        totalCount = data.totalCount
+        hasNextPage = data.pageInfo.hasNextPage;
+        after = data.pageInfo.endCursor;
+        for(let i=0;i<data.edges.length;i++){
+            let args = data.edges[i].node.dataParsed.args;
+            if(args && args.length==2){
+                totalStaked = BigNumber(totalStaked).plus(args[1].hex,16)
+                processedCount++;
+            }
+        }
+        const progress = Math.floor((processedCount / totalCount) * 100);
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            process.stdout.write(`Progress: ${progress}%`);
+      } catch (error) {
+        console.error(error);
+        break;
+      }
     }
-})
+    console.log("Total Staked: ",totalStaked.toNumber()/1e18);
+  }
+  
+fetchEvents();
+
 
--- utils/graphql.js
+++ utils/graphql.js
@@ -1,35 +1,51 @@
 const axios = require('axios')
 const gqlEndpoint =  "https://squid.subsquid.io/reef-explorer/graphql";
 
-const query = `query StakeEventsNr($contractAddress: String!,$topic:String!) {
-    evmEvents(
-      where: {
-        contractAddress_eq: $contractAddress,
-        topic0_eq: $topic,
-        status_eq: Success
-      },
-      orderBy: id_ASC,
-      limit: 100
-    ) {
-      dataParsed
-    }
+const query0 = `query StakeEventsNr($contractAddress: String!,$topic:String!) {
+    evmEventsConnection(orderBy: id_ASC, first: 10, where: {contractAddress_eq: $contractAddress, topic0_eq: $topic}) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        edges {
+          node {
+            dataParsed
+          }
+        }
+        totalCount
+      }
   }`
 
-const getStakeEvents = (contractAddress,topic) => {
+const query1 = `query StakeEventsNr($contractAddress: String!,$topic:String!,$after:String!) {
+    evmEventsConnection(orderBy: id_ASC, first: 10, where: {contractAddress_eq: $contractAddress, topic0_eq: $topic}, after: $after) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        edges {
+          node {
+            dataParsed
+          }
+        }
+        totalCount
+      }
+  }`
+
+const getStakeEvents = (contractAddress,topic,after) => {
     return {
-      query,
-      variables: { contractAddress,topic },
+      query : after=="0"?query0:query1,
+      variables: { contractAddress,topic,after },
     };
   };
 
-const fetchStakeEvents =async(contractAddress,topic)=> {
+const fetchStakeEvents =async(contractAddress,topic,after)=> {
     const response = await axios({
     method: "post",
     url: gqlEndpoint,
     headers: {
       "Content-Type": "application/json",
     },
-    data: getStakeEvents(contractAddress,topic),
+    data: getStakeEvents(contractAddress,topic,after),
   })
   return response.data;
 }
