da62b6c42d2c3c5cb64e2de9c8f476597ba08477
Anukul Pandey <anukul.030601@gmail.com>
2023-09-22 10:12:54 +0530

    feat: fetch evm events data

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const {argv} = require('./args');
+const {argv} = require('./utils/args');
 const {fetchStakeEvents} = require('./utils/graphql')
 
 const contractAddress = argv['contract-address'];
 const topic = argv['topic'];
 
 const response = fetchStakeEvents(contractAddress,topic);
-response.then(data=>console.log(data))
+response.then(data=>{
+    const evmEvents = data.data.evmEvents;
+    for(let i=0;i<evmEvents.length;i++){
+        console.log(evmEvents[i].dataParsed.args[1])
+    }
+})
 
